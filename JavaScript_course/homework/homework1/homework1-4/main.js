225dfee30712939c5c396040d45f63009f5d0f44
Sem-mi01 <semmik.ys@gmail.com>
2025-06-09 02:16:37 +0400

    made task1-4.html main.js

---
+++ JavaScript_course/homework/homework1/homework1-4/main.js
@@ -0,0 +1,11 @@
+// #ruUtWDUI
+//
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+let Name = prompt('Your name','Yuliia');
+console.log(Name);
+let lastName = prompt('your last name','Vasilievna');
+console.log(lastName);
+let Age = prompt('your age', 41);
+console.log(Age);
+
