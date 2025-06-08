29d54e9747e22076ca795e5a48d70d2c09056c20
Sem-mi01 <semmik.ys@gmail.com>
2025-06-09 01:44:02 +0400

    made task1-2.html main.js for homework 1

---
+++ JavaScript_course/homework/homework1/homework1-2/main.js
@@ -0,0 +1,10 @@
+// #6Qb97gsv
+//
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+
+let firstName = 'Erdogan';
+let middleName = 'Yullia';
+let lastName = 'Vasilievna';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
