97ba785d828dff656f964fbfc3435daa79dd03bc
Sem-mi01 <semmik.ys@gmail.com>
2025-06-09 02:18:42 +0400

    change main.js in homework1-4

--- JavaScript_course/homework/homework1/homework1-4/main.js
+++ JavaScript_course/homework/homework1/homework1-4/main.js
@@ -2,7 +2,7 @@
 //
 // – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
 
-let Name = prompt('Your name','Yuliia');
+let Name = prompt('Your name',' Yuliia');
 console.log(Name);
 let lastName = prompt('your last name','Vasilievna');
 console.log(lastName);
