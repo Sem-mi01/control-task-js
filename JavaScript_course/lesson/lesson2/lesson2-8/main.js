2066e6c2b122080f518dfe61ec44c10e5a807019
Sem-mi01 <semmik.ys@gmail.com>
2025-06-10 01:07:59 +0400

    filling if.html main.js

--- JavaScript_course/lesson/lesson2/lesson2-8/main.js
+++ JavaScript_course/lesson/lesson2/lesson2-8/main.js
@@ -1,16 +1,74 @@
+if (true) {
+    console.log('test');
+}// test
+if (false) {
+    console.log('test');
+}// не зреагувало
 
-// У JavaScript є 7 примітивних типів даних: string, number, bigint, boolean, undefined, null та symbol. Ці типи не є об'єктами і не мають методів, а можуть лише зберігати одне значення.
 
-let a = 100;
-let b = a;
-b = b + 10;
-console.log(a);  // 100
-console.log(b);  // 110
+// let color = 'red'
+// if (color === 'red') {
+//     console.log('stop');
+// } //stop
 
-// (reference type) посилальний - це не один із типів даних, а скоріше, як специфікація того, як значення присвоюється. Коли ви присвоюєте об'єкт одній змінній, потім інший змінній, то обидві змінні будуть посилатися (вказувати) на один об'єкт в пам'яті.
-let user = {name:'Vasya'};
-let user2 = user;
-console.log(user2);
-user2.age = 31;
-console.log(user2);
-console.log(user);
+// let color = 'green'
+// if (color === 'red') {
+//     console.log('stop'); // оскільки колір червоний, а не зелений, то цей вираз є false і він ігнорується і переходимо до наступного сегменту
+// }else {
+//     console.log('go'); // true
+// } // go
+
+// let age = +prompt('enter your age');
+// // console.log(age, typeof age);  // 41 string, але age це number
+// console.log(age, typeof +age); // 41 number, але перше age залишається string, тому + потрібно ставити там де age створюється
+
+// let age = +prompt('enter your age');
+// if (age >= 18) {
+//     console.log('adult');
+// }else {
+//     console.log('cartoon');
+// }
+
+// let color = prompt('enter color');
+// if (color === 'green') {
+//     if (confirm('is road clear')) {
+//         console.log('go');
+//     }else {
+//         console.log('just wait');
+//     }
+// }else if (color === 'yellow') {
+//     console.log('wait');
+// }else if (color === 'red') {
+//     console.log('stop');
+// }else {
+//     console.log('??????'); // якщо колір не відомий
+// }
+
+// let color = prompt('enter color');
+// if (color === 'green' && confirm('is road clear')) {
+//         console.log('go'); // при такому відображенні неможливо обробити варіант else
+// }else if (color === 'yellow') {
+//     console.log('wait');
+// }else if (color === 'red') {
+//     console.log('stop');
+// }else {
+//     console.log('??????'); // якщо колір не відомий
+// }
+
+// можна is road clean  винести в окрему змінну (option+command+v). результата аналогічний.
+
+let color = prompt('enter color');
+let isRoadClear = confirm('is road clear');
+if (color === 'green' && isRoadClear) {
+    console.log('go'); // при такому відображенні неможливо обробити варіант else
+}else if (color === 'yellow') {
+    console.log('wait');
+}else if (color === 'red') {
+    console.log('stop');
+}else {
+    console.log('??????'); // якщо колір не відомий
+}
+// можна міксувати різні варіанти. В else if також можна додавати внутрішні елементи else if, проте в else можна доавати тільки внутрішні if/
+
+
+
