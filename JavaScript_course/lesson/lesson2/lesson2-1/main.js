379520ee32b57f6514ae3564552e34e531d76ffa
Sem-mi01 <semmik.ys@gmail.com>
2025-06-09 13:59:34 +0400

    filling lesson2-1 main.js

---
+++ JavaScript_course/lesson/lesson2/lesson2-1/main.js
@@ -0,0 +1,21 @@
+let arr;
+arr = [123, 234, 345, -123, 0, true, 'asdfgghj'];  // кожен елемент є проіндексованим. Індексація починається з 0. в масиви можна додавати number, script, boolean
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[4]);
+console.log(arr.length);
+
+arr[0] = 'new valeu'; // перевизначення елементу
+console.log(arr);
+
+arr[7] = 'okten'; // додавання елементу в кінець елементу. [7] це індекс елемента
+console.log(arr);
+
+arr[arr.length] = 12983; // додавання елементу в кінець елементу, не обраховуючи кількість елементів. [arr.lenght] це gjnjxyf ljd;byf vfcbde
+console.log(arr);
+
+arr[arr.length] = 'asdgkhg';
+console.log(arr);
+
+
