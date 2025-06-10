d665649e58a491e37c5c362ca70317e12a69403c
Sem-mi01 <semmik.ys@gmail.com>
2025-06-10 21:19:24 +0400

    made task2-4.html, main.js

---
+++ JavaScript_course/homework/homework2/homework2-4/main.js
@@ -0,0 +1,24 @@
+// – Створити масив з 10 об’єктами, які описують сутніть “користувач”. Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let users = [
+    {name: 'Anna', username: 'Annet-01', password: '12345678'},
+    {name: 'Vasya', username: 'Vasya-24', password: 'hd456789F'},
+    {name: 'Olya', username: 'Olya-76', password: '87654321'},
+    {name: 'Ivan', username: 'Vano-43', password: 'Hgfqwe76'},
+    {name: 'Masha', username: 'Mary-23', password: 'Khgrs875*'},
+    {name: 'Katya', username: 'Katrin-87', password: '876Hjfdsr'},
+    {name: 'Dasha', username: 'Darina', password: 'khg_hgf65'},
+    {name: 'Denis', username: 'Den-567', password: 'kljhfgeousf'},
+    {name: 'Pavlo', username: 'Pasha32', password: 'lKh98786509'},
+    {name: 'Lena', username: 'lenok', password: 'ljohuigj98'}
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
