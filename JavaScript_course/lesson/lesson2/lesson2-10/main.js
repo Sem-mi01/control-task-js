ed435975fe282566a374484840e923a553be262c
Sem-mi01 <semmik.ys@gmail.com>
2025-06-10 18:01:20 +0400

    add lesson2-10 Ternary.html main.js

---
+++ JavaScript_course/lesson/lesson2/lesson2-10/main.js
@@ -0,0 +1,19 @@
+let obj = {
+    id: 1,
+    name: "Vasya",
+    age: 17
+}
+
+// let access;
+// // let condition = obj.age > 18;
+// if (obj.age > 18) {
+//     access = true;
+// } else {
+//     access = false
+// }
+//
+// console.log(access);
+
+let access = obj.age > 18 ? 'Yes' : 'No';
+
+console.log(access);
