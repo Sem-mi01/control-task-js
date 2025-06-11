4976e3bce848c635450f19b1c4215838f9913d6d
Sem-mi01 <semmik.ys@gmail.com>
2025-06-11 12:37:13 +0400

    add option let in main.js sy homework2-9

--- JavaScript_course/homework/homework2/homework2-9/main.js
+++ JavaScript_course/homework/homework2/homework2-9/main.js
@@ -2,30 +2,42 @@
 //
 // – Скласти розклад на тиждень за домопогою switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа, що заплановано на цей день (можна замість плану на день, назву дня англійською).
 
-let day = prompt('Enter the day of the week');
+// let day = prompt('Enter the day of the week');
+// можна так:
+let day = '1';
 
 switch (day) {
     case '1':
         console.log('Monday');
+        console.log('go to work');
         break;
     case '2':
         console.log('Tuesday');
+        console.log('go to work');
         break;
     case '3':
         console.log('Wednesday');
+        console.log('go to work');
         break;
     case '4':
         console.log('Thursday');
+        console.log('go to work');
         break;
     case '5':
         console.log('Friday');
+        console.log('go to work');
         break;
     case '6':
         console.log('Saturday');
+        console.log('weekend');
         break;
     case '7':
         console.log('Sunday');
+        console.log('weekend');
         break;
     default:
         console.log('??????');
 }
+
+
+
