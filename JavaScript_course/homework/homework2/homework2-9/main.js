63d317d0e8cb02793043ebcb9fe8bb87a93a52cd
Sem-mi01 <semmik.ys@gmail.com>
2025-06-11 11:55:45 +0400

    made homework2-9, task2-9.html main.js

---
+++ JavaScript_course/homework/homework2/homework2-9/main.js
@@ -0,0 +1,31 @@
+// #KzrtqyQ
+//
+// – Скласти розклад на тиждень за домопогою switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа, що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+let day = prompt('Enter the day of the week');
+
+switch (day) {
+    case '1':
+        console.log('Monday');
+        break;
+    case '2':
+        console.log('Tuesday');
+        break;
+    case '3':
+        console.log('Wednesday');
+        break;
+    case '4':
+        console.log('Thursday');
+        break;
+    case '5':
+        console.log('Friday');
+        break;
+    case '6':
+        console.log('Saturday');
+        break;
+    case '7':
+        console.log('Sunday');
+        break;
+    default:
+        console.log('??????');
+}
