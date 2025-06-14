a634c7a86139b349456e597952f0b53db61e6d0a
Sem-mi01 <semmik.ys@gmail.com>
2025-06-14 17:25:26 +0400

    made homework3-add5 task1.html, main.js

---
+++ JavaScript_course/homework/homework3/homework3-add/homework3-add5/main.js
@@ -0,0 +1,11 @@
+// #GamKju89ob
+//
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+
+let words = ['house', 'apartment', 'street', 'shop', 'table', 'chair', 'cup', 'plate', 'lamp', 'window'];
+let i = 0;
+while (i < words.length) {
+    let word = words[i];
+    console.log(word);
+    i++;
+}
