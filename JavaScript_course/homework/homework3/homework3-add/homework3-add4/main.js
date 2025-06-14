70458d1b870122f77810fd14bd697f0cc5e49740
Sem-mi01 <semmik.ys@gmail.com>
2025-06-14 17:04:30 +0400

    made homework3-add4 task1.html, main.js

---
+++ JavaScript_course/homework/homework3/homework3-add/homework3-add4/main.js
@@ -0,0 +1,9 @@
+// #yHAwJOyiC
+//
+// – Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+
+let number = [4, 67, 467, 9876, 76543, 907543, 5428765, 69874321,123456789]
+for (let i = 0; i < number.length; i++) {
+    const numberElement = number[i];
+    console.log(`${numberElement}`);
+}
