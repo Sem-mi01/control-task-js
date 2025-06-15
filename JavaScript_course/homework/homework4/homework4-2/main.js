a3026ddd012adf3a872c2531ca21f69b5747a8fb
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 01:58:24 +0400

    made homework4-2

---
+++ JavaScript_course/homework/homework4/homework4-2/main.js
@@ -0,0 +1,12 @@
+// #ETGAxbEn8l
+//
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+function radius (d) {
+    const result = d/2;
+    console.log(result);
+    return result;
+}
+    let d = 50;
+
+    radius(d)
