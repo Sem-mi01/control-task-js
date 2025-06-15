d6f2457162ce85dd102f1b51a2533c171389a28c
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 03:41:36 +0400

    made homework4-4

---
+++ JavaScript_course/homework/homework4/homework4-4/main.js
@@ -0,0 +1,9 @@
+// #SIdMd0hQ
+//
+// – створити функцію, яка приймає масив та виводить кожен його елемент
+
+function printerList (list) {
+    for (const item of list ) {
+        console.log(item);
+    }
+}
