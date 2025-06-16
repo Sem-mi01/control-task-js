a7d864ef7458649fbc17c280930bca0725cc391a
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 14:29:59 +0400

    made homework4-5

---
+++ JavaScript_course/homework/homework4/homework4-5/main.js
@@ -0,0 +1,12 @@
+// #59g0IsA
+//
+// – створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+function madeParagraph (text) {
+    // let p = `<p>${text}</p>`
+    // document.write(p);
+    document.write(`<p>${text}</p>`);
+}
+
+madeParagraph('lorem  ipsum');
+
