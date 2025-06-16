0f05729eb26092e00a0cf69a1d83ab39c1bd4354
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 22:51:08 +0400

    made homework4-13

---
+++ JavaScript_course/homework/homework4/homework4-13/main.js
@@ -0,0 +1,16 @@
+
+// Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+//
+// Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+
+function exchange(sumUAH,currencyValues,exchangeCurrency){
+    for(let i= 0; i < currencyValues.length; i++){
+        if(currencyValues[i].currency === exchangeCurrency){
+            return sumUAH / currencyValues[i].value;
+        }
+    }
+}
+console.log (exchange(10000,[
+    {currency:'USD',value:25},
+    {currency:'EUR',value:42}
+],'USD'));
