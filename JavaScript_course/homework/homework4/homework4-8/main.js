bb4484db0d3069d05fc072633e9159672bdfe2b6
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 17:17:44 +0400

    change main.js, in homework4-8

--- JavaScript_course/homework/homework4/homework4-8/main.js
+++ JavaScript_course/homework/homework4/homework4-8/main.js
@@ -1,19 +1,4 @@
 
-// #0Kxco1edSN
-//
-// – створити функцію, яка створює ul з  елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) та виводить його через document.write
-
-// function listText(text, number) {
-//     document.write('<ul>');
-//     for (let i = 0; i < number; i++) {
-//         document.write(`<li>${text}</li>`);
-//     }
-//      document.write('</ul>');
-// }
-// listText('lorem ipsum', 100);
-
-// #gEFoxMMO
-//
 // – створити функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список (ul li) та виводить його через document.write
 
 function primElements(arrayElements) {
