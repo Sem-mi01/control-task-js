235c5bc75b8b04db1f0048b6face3596fa5fb717
Sem-mi01 <semmik.ys@gmail.com>
2025-06-16 23:00:53 +0400

    made homework4-13

--- JavaScript_course/homework/homework4/homework4-13/main.js
+++ JavaScript_course/homework/homework4/homework4-13/main.js
@@ -3,14 +3,28 @@
 //
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
 
-function exchange(sumUAH,currencyValues,exchangeCurrency){
-    for(let i= 0; i < currencyValues.length; i++){
-        if(currencyValues[i].currency === exchangeCurrency){
-            return sumUAH / currencyValues[i].value;
+// function exchange(sumUAH,currencyValues,exchangeCurrency){
+//     for(let i= 0; i < currencyValues.length; i++){
+//         if(currencyValues[i].currency === exchangeCurrency){
+//             return sumUAH / currencyValues[i].value;
+//         }
+//     }
+// }
+// console.log (exchange(10000,[
+//     {currency:'USD',value:25},
+//     {currency:'EUR',value:42}
+// ],'USD'));
+
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    let chosenCurrency;
+    for (const item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            chosenCurrency = item;
         }
     }
+    let result = sumUAH/chosenCurrency.value
+    return result;
 }
-console.log (exchange(10000,[
-    {currency:'USD',value:25},
-    {currency:'EUR',value:42}
-],'USD'));
+
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
