931d92b889e1904d9d93241526d748d03892fde7
Sem-mi01 <semmik.ys@gmail.com>
2025-06-23 16:31:28 +0400

    look task4-13.html main.js

--- JavaScript_course/homework/homework4/homework4-13/main.js
+++ JavaScript_course/homework/homework4/homework4-13/main.js
@@ -28,3 +28,4 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
 
 console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
 console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
+
