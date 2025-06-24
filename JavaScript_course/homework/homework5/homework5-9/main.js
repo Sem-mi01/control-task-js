49bae730a0e85dbd305545b2f62a5343b0c65fa3
Sem-mi01 <semmik.ys@gmail.com>
2025-06-24 17:43:01 +0400

    made homework 5-9 task5-9.html main.js

---
+++ JavaScript_course/homework/homework5/homework5-9/main.js
@@ -0,0 +1,17 @@
+// #bovDJDTIjt
+//
+// – створити cтрілочну функцію, яка приймає масив об’єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об’єкту окремий блок.
+
+let usersArray = (users) => {
+    for (const user of users) {
+        document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
+    }
+}
+
+usersArray([
+    {id: 1, name: 'John', age: 15},
+    {id: 2, name: 'Julia', age: 25},
+    {id: 3, name: 'Silva', age: 32},
+    {id: 4, name: 'Slava', age: 40},
+    {id: 5, name: 'Maks', age: 50},
+]);
