729faf2b8187840ba2e29db32d1ec48e19dc059d
Sem-mi01 <semmik.ys@gmail.com>
2025-06-24 15:00:14 +0400

    made homework 5-5 task5-5.html main.js

---
+++ JavaScript_course/homework/homework5/homework5-5/main.js
@@ -0,0 +1,21 @@
+// #59g0IsA
+//
+// – створити стрілочну функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+// function madeParagraph (text) {
+//     // let p = `<p>${text}</p>`
+//     // document.write(p);
+//     document.write(`<p>${text}</p>`);
+// }
+//
+// madeParagraph('lorem  ipsum');
+
+let madParagraph = (text) => {document.write(`<p>${text}</p>`)}
+
+madParagraph('lorem  ipsum') ;
+
+
+// let maddeParagraph = (text) => {console.log(`${text}`)}
+// maddeParagraph('lorem  ipsum');
+
+
