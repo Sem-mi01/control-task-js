ba8dbd4bdff64ac13eedab7d71a8d41c4cb928f2
Sem-mi01 <semmik.ys@gmail.com>
2025-06-29 17:31:21 +0400

    made homework6-8, task6-8.html, main.js

---
+++ JavaScript_course/homework/homework6/homework6-8/main.js
@@ -0,0 +1,42 @@
+// #yo06d74c1C
+
+// – є масив
+//
+// let coursesAndDurationArray = [
+//
+//     {title: ‘JavaScript Complex’, monthDuration: 5},
+//
+// {title: ‘Java Complex’, monthDuration: 6},
+//
+// {title: ‘Python Complex’, monthDuration: 6},
+//
+// {title: ‘QA Complex’, monthDuration: 4},
+//
+// {title: ‘FullStack’, monthDuration: 7},
+//
+// {title: ‘Frontend’, monthDuration: 4}
+//
+// ];
+//
+//  — відсортувати його за спаданням за monthDuration
+//
+//  — відфільтрувати, залишивши тільки курси з тривалістю понад 5 місяців
+//
+//  — за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+const sort =  coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(sort);
+
+const filter= sort.filter(value => value.monthDuration > 5);
+console.log(filter);
+
+console.log(filter.map((value, index) => ({...value, id: index + 1
+})));
