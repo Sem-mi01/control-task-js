0440ebb7be1e4e1f6dbebc4108648518c9d1ff58
Sem-mi01 <semmik.ys@gmail.com>
2025-07-18 23:19:53 +0400

    made task9-5.html, style.css,

---
+++ JavaScript_course/homework/homework9/homework9-5/main.js
@@ -0,0 +1,96 @@
+// – Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+//
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+for (const courses of coursesArray) {
+    let courseDiv = document.createElement('div');
+    courseDiv.classList.add('course');
+
+    let h2 = document.createElement('h2');
+    h2.innerText = courses.title;
+
+    let h3 = document.createElement('h3');
+    h3.innerText = courses.monthDuration;
+
+    let p = document.createElement('p');
+    p.innerText = courses.hourDuration;
+
+    let ul = document.createElement('ul');
+    for (const modulesElement of courses.modules) {
+        let li = document.createElement('li');
+        li.innerText = modulesElement;
+        ul.appendChild(li);
+        }
+    courseDiv.append(h2, h3, p, ul);
+    document.body.appendChild(courseDiv);
+}
