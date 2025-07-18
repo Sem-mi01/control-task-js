dc63be28a1f468760f6854a0527851691cbe0974
Sem-mi01 <semmik.ys@gmail.com>
2025-07-18 22:40:00 +0400

    made task9-4.html, style.css,

---
+++ JavaScript_course/homework/homework9/homework9-4/main.js
@@ -0,0 +1,28 @@
+// #Kx1xgoKy8
+//
+// – Є масив
+//
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class=’item’> ,  в якому буде <h1 class=’heading’>  з title  елементу, та <p class=’description’> з monthDuration елементу.
+//
+//     Завдання робити через цикли.
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const course of coursesAndDurationArray) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.innerHTML = course.title;
+    const p = document.createElement('p');
+    p.innerHTML = course.monthDuration;
+    div.append(h2, p);
+    document.body.appendChild(div);
+}
