fd58315eb6ae7d23c19788ca6da02247e997ab28
Sem-mi01 <semmik.ys@gmail.com>
2025-07-20 14:45:33 +0400

    add homework10-1, task10-1.html, main.js

--- JavaScript_course/homework/homework10/homework10-1/main.js
+++ JavaScript_course/homework/homework10/homework10-1/main.js
@@ -12,3 +12,19 @@ document.body.append(div, button);
 button.onclick = function () {
     document.getElementById("text").remove();
 }
+
+// ще варіанти згідно відео дз
+// const buttons = document.getElementsByTagName("button");
+// const button1 = buttons[0];
+
+// або ще варіант
+const button1 = document.getElementById("btn");
+
+button1.onclick = function () {
+    // document.getElementById("text1").remove();
+    // або
+    // document.getElementById("text1").style.height = 0;
+    // або
+    document.getElementById("text1").style.display = 'none';
+
+}
