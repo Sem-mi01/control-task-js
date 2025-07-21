4f8f3d4f4aba18c66365f1432499d7f217dc8984
Sem-mi01 <semmik.ys@gmail.com>
2025-07-21 04:21:10 +0400

    made homework10-10, task10-10.html, main.js,

---
+++ JavaScript_course/homework/homework10/homework10-10/main.js
@@ -0,0 +1,49 @@
+// #NKB0tgWIK1G
+//
+// ***PAGINATION
+//
+// зробити масив на 100 об’єктів та дві кнопки prev next
+//
+// при завантаженні сторінки з’являються перші 10 об’єктів.
+//
+//     При натисканні next виводяться наступні 10 об’єктів
+//
+// При натисканні prev виводяться попередні 10 об’єктів
+
+const arr = Array.from({length: 100}, (_, i) => `obj ${i+1}`);
+
+const itemsPerPage = 10;
+let currentPage = 1;
+
+const arrayDiv = document.getElementById('array');
+const prevButton     = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+function displayItems() {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentItems = data.slice(startIndex, endIndex);
+    arrayDiv.innerHTML = '';
+
+    currentItems.forEach((item => {
+        const p = document.createElement('p');
+        p.textArray = item;
+        arrayDiv.appendChild(p);
+    }));
+    prevButton.disabled = currentPage === 1;
+    nextButton.disabled = endIndex >= data.length;
+}
+prevButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+
+    }
+});
+nextButton.addEventListener('click', () => {
+    const maxPages = Math.ceil(data.length / itemsPerPage);
+    if (currentPage < maxPages) {
+        currentPage++;
+        displayItems();
+    }
+});
+document.addEventListener('DOMContentLoaded', displayItems);
