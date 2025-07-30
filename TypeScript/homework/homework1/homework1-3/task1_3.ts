345a584740857e1a4739526c4a775641d2b619f8
Sem-mi01 <semmik.ys@gmail.com>
2025-07-31 00:56:01 +0400

    made homework 1 task1-2.html, task1_2.ts

---
+++ TypeScript/homework/homework1/homework1-3/task1_3.ts
@@ -0,0 +1,14 @@
+// #4N0y5tufA
+//
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//
+//     let a = 100; let b = ‘100’; let c = true;
+
+let aa: number = 100;
+console.log(typeof aa);
+
+let bb: string = '100';
+console.log(typeof bb);
+
+let cc: boolean = true;
+console.log(typeof cc);
