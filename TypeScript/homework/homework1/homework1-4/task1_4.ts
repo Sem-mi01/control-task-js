d644aa175174af2810747f7e52090131e22daaa5
Sem-mi01 <semmik.ys@gmail.com>
2025-07-31 00:57:18 +0400

    made homework 1 task1-4.html, task1_4.ts

---
+++ TypeScript/homework/homework1/homework1-4/task1_4.ts
@@ -0,0 +1,11 @@
+// #ruUtWDUI
+//
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+let Name: string| null = prompt('Your name',' ');
+console.log(Name);
+let midName: string| null = prompt('your last name',' ');
+console.log(midName);
+let age: string| null = prompt('your age', ' ');
+console.log(typeof age, age);
+
