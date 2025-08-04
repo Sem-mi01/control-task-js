709e4ac5e46e95612a6b43413fd7a6d365702e93
Sem-mi01 <semmik.ys@gmail.com>
2025-08-04 20:29:51 +0400

    made homework 3 task3-4.html, task3_4.ts

---
+++ TypeScript/homework/homework3/homework3-4/task3_4.ts
@@ -0,0 +1,9 @@
+// #OeT7t3uUMFi
+//
+// – За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let qwe: number = 0;
+while (qwe < 20) {
+    document.write(`<h1>Lorem ipsum dolor sit - ${i}</h1>`);
+    qwe++;
+}
